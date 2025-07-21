50991b88fadcdbe71b26cb74922275ddccfb3564
Ghulam Ahmed <dev.ahmed47@gmail.com>
2025-07-21 18:54:43 +0500

    fixing black mode colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
   const bgColor =
     theme === "dark"
-      ? "bg-[#202020] text-gray-300"
+      ? "bg-black text-gray-300"
       : "bg-[#ffffff] text-gray-700";
 
   const headingColor = theme === "dark" ? "text-white" : "text-black";
